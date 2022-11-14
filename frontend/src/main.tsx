c51d71a8bca55b646eb82af00d661beea2d6e0e3
zsr12383 <zsr123@naver.com>
2022-11-14 14:52:13 +0900

    feat: 브라우저 라우터, 절대 경로 설정, 전역 스타일 적용

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,8 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
+import {BrowserRouter} from "react-router-dom";
+import {RecoilRoot} from 'recoil';
+import {GlobalStyle} from "@src/styles/GlobalStyle";
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <GlobalStyle/>
+        <RecoilRoot>
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
+        </RecoilRoot>
+    </React.StrictMode>
 )
