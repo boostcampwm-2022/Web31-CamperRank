70ac8a3c253af40167d1b51fadd287dc7eac305a
Daepoid <daepoid@gmail.com>
2022-11-14 20:18:01 +0900

    feat: swagger 추가

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,25 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { User } from './users/entities/user.entity';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      // envFilePath: './config/env/.database.env',
-      // envFilePath: '.database.env',
-    }),
+    // ConfigModule.forRoot({
+    //   envFilePath: '.env',
+    // }),
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
       port: 3306,
-      username: '', // MySQL ID
-      password: '', // MySQL password
+      username: 'root', // MySQL ID
+      password: 'root', // MySQL password
       database: 'camperRank',
       entities: [User],
-      synchronize: false, // 처음에만 true로 설정
+      synchronize: false, // synchronize 옵션을 true로 하면 서비스가 실행되고 데이터베이스가 연결될 때 항상 데이터베이스가 초기화 되므로 절대 프로덕션에는 false로 설정
     }),
     UsersModule,
   ],
