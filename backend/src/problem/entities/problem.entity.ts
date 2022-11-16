588fe11259f5f557e1a4fac2753e2532a8904735
Daepoid <daepoid@gmail.com>
2022-11-16 14:31:16 +0900

    chore: problem entity 설계 변경

--- backend/src/problem/entities/problem.entity.ts
+++ backend/src/problem/entities/problem.entity.ts
@@ -12,44 +12,20 @@ export class Problem extends BaseTimeEntity {
   title: string;
 
   @Column()
-  description: string;
-
-  @Column({ name: 'input_format' })
-  inputFormat: string;
+  level: number;
 
   @Column()
-  constraints: string;
-
-  @Column({ name: 'sample_input' })
-  sampleInput: string;
-
-  @Column({ name: 'sample_output' })
-  sampleOutput: string;
-
-  @Column({ name: 'sample_explanation' })
-  sampleExplanation: string;
+  description: string;
 
   testcaseList: TestCase[];
 
   solvedList: Solved[];
 
-  public static createProblem({
-    title,
-    description,
-    inputFormat,
-    constraints,
-    sampleInput,
-    sampleOutput,
-    sampleExplanation,
-  }) {
+  public static createProblem({ title, level, description }) {
     const problem = new Problem();
     problem.title = title;
+    problem.level = level;
     problem.description = description;
-    problem.inputFormat = inputFormat;
-    problem.constraints = constraints;
-    problem.sampleInput = sampleInput;
-    problem.sampleOutput = sampleOutput;
-    problem.sampleExplanation = sampleExplanation;
     return problem;
   }
 }
