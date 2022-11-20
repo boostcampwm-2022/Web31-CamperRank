7528b4fe3f4007d5504d873dc098e6d9efea643c
Daepoid <daepoid@gmail.com>
2022-11-20 11:09:06 +0900

    feat: env 세팅 추가

--- backend/src/config/typeorm.config.ts
+++ backend/src/config/typeorm.config.ts
@@ -1,16 +1,20 @@
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { User } from '../users/entities/user.entity';
-import { Problem } from '../problem/entities/problem.entity';
-import { TestCase } from '../test-case/entities/test-case.entity';
-import { Solved } from '../solved/entities/solved.entity';
+// export const typeormConfig: TypeOrmModuleOptions = {
+//   type: 'mysql',
+//   host: 'localhost',
+//   port: 3306,
+//   username: 'root', // MySQL ID
+//   password: 'root', // MySQL password
+//   database: 'camperRank',
+//   entities: [User, Problem, TestCase, Solved],
+//   synchronize: false, // synchronize 옵션을 true로 하면 서비스가 실행되고 데이터베이스가 연결될 때 항상 데이터베이스가 초기화 되므로 절대 프로덕션에는 false로 설정
+// };
 
-export const typeormConfig: TypeOrmModuleOptions = {
-  type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: '', // MySQL ID
-  password: '', // MySQL password
-  database: 'camperRank',
-  entities: [User, Problem, TestCase, Solved],
-  synchronize: false, // synchronize 옵션을 true로 하면 서비스가 실행되고 데이터베이스가 연결될 때 항상 데이터베이스가 초기화 되므로 절대 프로덕션에는 false로 설정
-};
+export const typeormConfig = () => ({
+  database: {
+    host: process.env.MYSQL_HOST || '',
+    port: process.env.MYSQL_PORT || '',
+    username: process.env.MYSQL_USERNAME || '',
+    password: process.env.MYSQL_PASSWORD || '',
+    database: process.env.MYSQL_DATABASE || '',
+  },
+});
