158a9344a928aa1353e915e35af6116268d74c9a
sinterhoo <56079377+sinterhoo@users.noreply.github.com>
2022-11-22 17:58:41 +0900

    feat : 자바스크립트 채점

--- gradeServer/src/controllers/gradeController.js
+++ gradeServer/src/controllers/gradeController.js
@@ -7,11 +7,16 @@ exports.startGrade = async function (req, res) {
     try {
         let { userCode, language, input, output } = req.body;
         let filePath = '';
+        let codeStyle = '';
+        let fileList = [];
         const fileName = Math.floor(Math.random() * 100000000);
+
         if (language === 'Python') {
             const pythonText = fs.readFileSync(__dirname+'/python.txt', 'utf-8');
+            codeStyle = 'python';
             filePath = __dirname +'../../../'+ fileName + '.py';
             userCode = 'import sys \n\n' + userCode +'\n\n' + pythonText;
+            fileList.push(filePath);
             
 
             if (input !== undefined) {
@@ -20,6 +25,25 @@ exports.startGrade = async function (req, res) {
                         userCode += ','
                     }
                     userCode += `sys.argv[${i+1}]`
+                    fileList.push(input[i]);
+                }
+            }
+            userCode += ')';
+            console.log(userCode)
+
+        }
+        else if (language === 'JavaScript') {
+            codeStyle = 'node';
+            filePath = __dirname +'../../../'+ fileName + '.js';
+            fileList.push(filePath);
+            userCode += '\n\n' + 'solution(';
+            if (input !== undefined) {
+                for (let i = 1; i < input.length+1; i++) {
+                    if (i !== 1) {
+                        userCode += ','
+                    }
+                    userCode += `process.argv[${i+1}]`
+                    fileList.push(input[i-1]);
                 }
             }
             userCode += ')';
@@ -35,13 +59,16 @@ exports.startGrade = async function (req, res) {
                 console.log('fail');
             }
         })
-        const pythonResult = spawnSync('python', [`${filePath}`,'1','2']);
-        const resultText = pythonResult.stdout.toString();
-        const errText = pythonResult.stderr.toString();
+        
+        const codeResult = spawnSync(codeStyle, fileList);
+        const resultText = codeResult.stdout.toString();
+        const errText = codeResult.stderr.toString();
+        console.log(resultText)
+        console.log(errText)
 
         fs.unlinkSync(filePath)
 
-        if (errText.length > 0) {
+        if (errText.length > 0 || errText === null) {
             return res.json({
                 isSuccess: false,
                 code: 2000,
