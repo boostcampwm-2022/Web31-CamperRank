f59b3d8cf39c161d93f4db04b130d3c83b34452a
Daepoid <daepoid@gmail.com>
2022-11-23 11:14:03 +0900

    chore: gradeServer 폴더 삭제

--- gradeServer/index.js
+++
@@ -1,5 +0,0 @@
-const express = require('./config/express');
-
-const port = 4000;
-express().listen(port);
-console.log(`${process.env.NODE_ENV} - API Server Start At Port ${port}`);
--- gradeServer/src/routes/gradeRoute.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(app){
-    const grade = require('../controllers/gradeController');
-
-    app.post('/grades',  grade.startGrade);
-
-};
