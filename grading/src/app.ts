680b55defb04f67d707b5e522bf2f2ca86df357d
Daepoid <daepoid@gmail.com>
2022-11-23 11:13:20 +0900

    chore: gradeServer 폴더 삭제

--- grading/src/app.ts
+++ grading/src/app.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import gradeRouter from "./routes/grade.route";
 import cors from "cors";
+import compression from "compression";
+import methodOverride from "method-override";
 
 const app = express();
 
@@ -10,6 +12,15 @@ const corsOptions = {
 };
 
 app.use(cors());
+
+// 미들웨어 압축, 파일 용량 줄임
+app.use(compression());
+// restful api 중 put, delete를 사용하기 위해 씀
+app.use(methodOverride());
+
+// urlencoded 페이로드로 들어오는 요청을 분석, extended true는 qs 모듈을 써서 body parsing
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.json());
 app.use("/", gradeRouter);
 
--- grading/src/controllers/grade.controller.ts
+++ grading/src/controllers/grade.controller.ts
@@ -29,7 +29,7 @@ function PLClassifier(pl: string) {
 //   }
 // }
 
-export const startGrade = async (req: any, res: any) => {
+export const gradingController = async (req: any, res: any) => {
   try {
     // const codeText = req.body.code;
     const userCode = req.body.data.userCode;
@@ -68,6 +68,87 @@ export const startGrade = async (req: any, res: any) => {
       solvedId: req.body.data.solvedId,
       result: "fail",
       msg: "채점 실패",
+    });
+  }
+};
+
+export const startGrade = async function (req: any, res: any) {
+  //
+  try {
+    let { userCode, language, input, output } = req.body;
+    let filePath = "";
+    let codeStyle = "";
+    let fileList = [];
+    const fileName = Math.floor(Math.random() * 100000000);
+
+    if (language === "Python") {
+      const pythonText = fs.readFileSync(__dirname + "/python.txt", "utf-8");
+      codeStyle = "python";
+      filePath = __dirname + "../../../" + fileName + ".py";
+      userCode = "import sys \n\n" + userCode + "\n\n" + pythonText;
+      fileList.push(filePath);
+
+      if (input !== undefined) {
+        for (let i = 0; i < input.length; i++) {
+          if (i !== 0) {
+            userCode += ",";
+          }
+          userCode += `sys.argv[${i + 1}]`;
+          fileList.push(input[i]);
+        }
+      }
+      userCode += "))";
+      console.log(userCode);
+    } else if (language === "JavaScript") {
+      codeStyle = "node";
+      filePath = __dirname + "../../../" + fileName + ".js";
+      fileList.push(filePath);
+      userCode += "\n\n" + "console.log(solution(";
+      if (input !== undefined) {
+        for (let i = 1; i < input.length + 1; i++) {
+          if (i !== 1) {
+            userCode += ",";
+          }
+          userCode += `process.argv[${i + 1}]`;
+          fileList.push(input[i - 1]);
+        }
+      }
+      userCode += "))";
+      console.log(userCode);
+    }
+
+    fs.writeFileSync(filePath, `${userCode}`);
+
+    const codeResult = spawnSync(codeStyle, fileList);
+    const resultText = codeResult.stdout.toString();
+    const errText = codeResult.stderr.toString();
+    console.log(resultText);
+    console.log(errText);
+
+    fs.unlinkSync(filePath);
+
+    if (errText.length > 0 || errText === null) {
+      return res.json({
+        isSuccess: false,
+        code: 2000,
+        message: "채점 실패",
+      });
+    } else {
+      return res.json({
+        result: resultText,
+        isSuccess: true,
+        code: 1000,
+        message: "채점 성공",
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    console.log(`Api - grade problem error\n: ${JSON.stringify(err)}`);
+
+    return res.json({
+      isSuccess: false,
+      code: 2000,
+      message: "채점 실패",
     });
   }
 };
