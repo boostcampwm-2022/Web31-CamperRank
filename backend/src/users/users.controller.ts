829aa4fb268e325aa89d03b329ad2109c05e16ee
Daepoid <daepoid@gmail.com>
2022-11-30 00:58:31 +0900

    feat: cross-validator 세팅 및 적용
    @Query의 경우에는 입력 값의 검증을 따로 하거나 분기처리가 필요

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Get, HttpStatus, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Post,
+  Query,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
@@ -12,6 +21,7 @@ export class UsersController {
   @Post()
   @ApiOperation({ summary: '유저 생성 API', description: '유저를 생성한다.' })
   @ApiResponse({ description: '유저를 생성한다.', type: SimpleUserDto })
+  @UsePipes(ValidationPipe)
   async create(@Body() createUserDto: CreateUserDto) {
     //TODO: cross-validator로 검증을 진행한다.
     const simpleUserDto = await this.usersService.create(createUserDto);
@@ -22,11 +32,20 @@ export class UsersController {
     };
   }
 
+  /**
+   * userId 또는 loginId 를 입력받아서 유저를 검색한다.
+   * 두 값이 모두 입력되지 않는 경우 typeorm 에서 undefined 조건으로 검색이 된다.
+   * 이 경우 의도하지 않게 SELECT * FROM users LIMIT 1; 의 쿼리를 날린 것과 같은 상태가 되어
+   * AutoIncrement 로 생성된 userId가 가장 작은 값이 나타나게 된다. (기본적으로 userId 가 1인 값이 반환된다.)
+   * @param userId
+   * @param loginId
+   * @return { statusCode, SimpleUserDto }
+   */
   @Get()
   @ApiOperation({ summary: '유저 검색 API', description: '유저를 검색한다.' })
   @ApiResponse({
     description:
-      '사용자 식별 아이디 또는 로그인 아이디를 받아 유저를 검색한다.',
+      '사용자 식별 아이디(userId) 또는 로그인 아이디(loginId)를 받아 유저를 검색한다.',
     status: HttpStatus.OK,
     type: SimpleUserDto,
   })
@@ -34,12 +53,17 @@ export class UsersController {
     @Query('userId') userId: number,
     @Query('loginId') loginId: string,
   ) {
-    // TODO: pipe 대신에 직접 number가 들어오는지 확인
+    if (!userId && !loginId) {
+      return {
+        statusCode: HttpStatus.BAD_REQUEST,
+      };
+    }
 
     const simpleUserDto = await this.usersService.findUser({
       userId: userId,
       loginId: loginId,
     });
+
     return {
       statusCode:
         simpleUserDto !== null ? HttpStatus.OK : HttpStatus.BAD_REQUEST,
