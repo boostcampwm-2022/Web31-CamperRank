3e3772e68e1a65a6db3b2101d36d3b057c43cb4a
Daepoid <daepoid@gmail.com>
2022-11-30 15:49:17 +0900

    feat: cross-validator 를 이용하여 처리 확인 및 userId 사용 부분 제거
    메서드 동작 설명 추가

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -12,19 +12,26 @@ import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { SimpleUserDto } from './dto/simple-user.dto';
+import { isFalsy } from '../utils/boolUtils';
 
 @Controller('users')
 @ApiTags('사용자 API')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  /**
+   * body 로 loginId 와 password 를 받는다.
+   * 이때 cross-validator 로 간단한 validation 을 수행한다.
+   * @param createUserDto
+   * @return { statusCode, SimpleUserDto }
+   */
   @Post()
   @ApiOperation({ summary: '유저 생성 API', description: '유저를 생성한다.' })
   @ApiResponse({ description: '유저를 생성한다.', type: SimpleUserDto })
   @UsePipes(ValidationPipe)
   async create(@Body() createUserDto: CreateUserDto) {
-    //TODO: cross-validator로 검증을 진행한다.
     const simpleUserDto = await this.usersService.create(createUserDto);
+
     return {
       statusCode:
         simpleUserDto !== null ? HttpStatus.OK : HttpStatus.BAD_REQUEST,
@@ -33,8 +40,8 @@ export class UsersController {
   }
 
   /**
-   * userId 또는 loginId 를 입력받아서 유저를 검색한다.
-   * 두 값이 모두 입력되지 않는 경우 typeorm 에서 undefined 조건으로 검색이 된다.
+   * loginId 를 입력받아서 유저를 검색한다.
+   * loginId가 undefined 인 경우 typeorm 에서 undefined 조건으로 검색이 된다.
    * 이 경우 의도하지 않게 SELECT * FROM users LIMIT 1; 의 쿼리를 날린 것과 같은 상태가 되어
    * AutoIncrement 로 생성된 userId가 가장 작은 값이 나타나게 된다. (기본적으로 userId 가 1인 값이 반환된다.)
    * @param userId
@@ -49,18 +56,14 @@ export class UsersController {
     status: HttpStatus.OK,
     type: SimpleUserDto,
   })
-  async findUser(
-    @Query('userId') userId: number,
-    @Query('loginId') loginId: string,
-  ) {
-    if (!userId && !loginId) {
+  async findUser(@Query('loginId') loginId: string) {
+    if (isFalsy(loginId)) {
       return {
         statusCode: HttpStatus.BAD_REQUEST,
       };
     }
 
-    const simpleUserDto = await this.usersService.findUser({
-      userId: userId,
+    const simpleUserDto = await this.usersService.findUserByLoginId({
       loginId: loginId,
     });
 
