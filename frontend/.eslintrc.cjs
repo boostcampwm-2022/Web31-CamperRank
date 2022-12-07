c6c9b10cc01a6bd8a1ce73ec8d7001c2b20a75fe
zsr12383 <zsr123@naver.com>
2022-12-07 19:37:07 +0900

    chore: 린트 설정 및 에디터 삭제

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,43 +1,43 @@
 module.exports = {
-    parser: "@typescript-eslint/parser",
-    settings: {
-        react: {
-            version: 'detect',
-        },
-        'import/resolver': {
-            node: {
-                paths: ['src'],
-                extensions: ['.js', '.jsx', '.ts', '.tsx'],
-            },
-        },
+  parser: '@typescript-eslint/parser',
+  settings: {
+    react: {
+      version: 'detect',
     },
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 13,
-        sourceType: "module",
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
     },
-    plugins: ["react", "@typescript-eslint"],
-    extends: [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        'plugin:jsx-a11y/recommended',
-        "plugin:@typescript-eslint/recommended",
-        "plugin:prettier/recommended",
-    ],
-    ignorePatterns: ['.eslintrc.cjs', 'build', 'dist', 'public', '*.svg', '.lock'],
-    env: {
-        browser: true,
-        es2021: true,
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    root: true,
-    rules: {
-        "@typescript-eslint/interface-name-prefix": "off",
-        "@typescript-eslint/explicit-function-return-type": "off",
-        "@typescript-eslint/explicit-module-boundary-types": "off",
-        "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/ban-ts-comment": "off",
-        'react/react-in-jsx-scope': 'off',
-    },
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['.eslintrc.cjs', 'build', 'dist', 'public', '*.svg', '.lock'],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  root: true,
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
 };
