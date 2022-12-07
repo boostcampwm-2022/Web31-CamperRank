97734c147fc05034e92de44b95666b967740d1fb
zsr12383 <zsr123@naver.com>
2022-12-07 11:53:41 +0900

    feat: 화상 시그널링 서버 로깅 로직 추가

--- socket/src/app.ts
+++ socket/src/app.ts
@@ -14,20 +14,26 @@ const io = new Server(httpServer, {
   cookie: true,
 });
 
+function logging(msg: string, param?: string) {
+  if (!param) {
+    console.log(`[${new Date().toLocaleString()}]  ${msg}`);
+    return;
+  }
+  console.log(`[${new Date().toLocaleString()}]  ${msg}: ${param}`);
+}
+
 io.on('connection', (socket) => {
-  console.log('user joined server');
+  logging('user joined server');
   socket.on('join-room', (roomId, userId) => {
-    if (
-      io.sockets.adapter.rooms.get(roomId) &&
-      io.sockets.adapter.rooms.get(roomId)!.size >= 3
-    ) {
-      console.log('full, out userId: ', userId);
+    const room = io.sockets.adapter.rooms.get(roomId);
+    if (room && room.size >= 3) {
+      logging('room is full, out userId: ', userId);
       socket.emit('full');
       return;
     }
 
-    console.log('join userId: ', userId);
-    console.log('roomId: ', roomId);
+    logging('join userId: ', userId);
+    logging('roomId: ', roomId);
 
     socket.join(roomId);
     socket.to(roomId).emit('user-connected', userId);
@@ -38,8 +44,8 @@ io.on('connection', (socket) => {
   });
 
   socket.on('change-webrtc', (roomId, userId) => {
-    console.log('change userId: ', userId);
-    console.log('roomId: ', roomId);
+    logging('change userId: ', userId);
+    logging('roomId: ', roomId);
     socket.to(roomId).emit('change-webrtc', userId);
   });
 });
