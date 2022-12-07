cc0f68cfcd5fbc8998e4151ee3569e7c4e8fe278
zsr12383 <zsr123@naver.com>
2022-12-08 03:41:15 +0900

    feat: 랭킹 페이지, 프로필 페이지 뼈대 작성, 라우팅 단에서 로그인 검사하도록 로직 변경

---
+++ frontend/src/pages/Ranking.tsx
@@ -0,0 +1,43 @@
+import { MainHeader } from '../components/MainHeader';
+import { Footer } from '../components/Footer';
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  width: 100%;
+  min-height: 66rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const HeaderWrapper = styled.div`
+  min-width: 80rem;
+  min-height: 8rem;
+`;
+
+const ContentWrapper = styled.div`
+  min-width: 80rem;
+  flex: 1;
+  background: #e4e8e0;
+`;
+
+const FooterWrapper = styled.div`
+  min-width: 80rem;
+  min-height: 20rem;
+`;
+
+export const Ranking = () => {
+  return (
+    <Wrapper>
+      <HeaderWrapper>
+        <MainHeader></MainHeader>
+      </HeaderWrapper>
+      <ContentWrapper></ContentWrapper>
+      <FooterWrapper>
+        <Footer />
+      </FooterWrapper>
+    </Wrapper>
+  );
+};
