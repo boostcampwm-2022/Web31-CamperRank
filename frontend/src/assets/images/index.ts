87a3e58947cf8c4044acc261a644c750cb27d86c
zsr12383 <zsr123@naver.com>
2022-12-13 18:38:52 +0900

    feat: png -> webp로 format 변경

--- frontend/src/assets/images/index.ts
+++ frontend/src/assets/images/index.ts
@@ -1,5 +1,5 @@
-import Banner1 from './Banner1.png';
-import Banner2 from './Banner2.png';
-import Banner3 from './Banner3.png';
+import Banner1 from './Banner1.webp';
+import Banner2 from './Banner2.webp';
+import Banner3 from './Banner3.webp';
 
 export { Banner1, Banner2, Banner3 };
