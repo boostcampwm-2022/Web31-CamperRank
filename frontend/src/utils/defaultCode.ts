6752c78fbdcb6972d8a4587717b6a16e3215c191
tommy1610 <tommy1610@naver.com>
2022-12-16 12:51:45 +0900

    fix: default python code 수정

--- frontend/src/utils/defaultCode.ts
+++ frontend/src/utils/defaultCode.ts
@@ -10,17 +10,14 @@ function solution(param) {
   return answer;
 
 }`,
-  Python: `/*
-함수 내부에 실행 코드를 작성하세요
-*/
+  Python: `# 함수 내부에 실행 코드를 작성하세요
 
 def solution(param):
 
   answer = 0
 
   return answer
-
-}`,
+`,
   '': `/*
 
 언어를 선택하세요
